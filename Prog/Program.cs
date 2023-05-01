b1a97a91ad5387ad9e9d63d986bd36f9af75b6e2
Arve Svendsen <arve.svendsen@gmail.com>
2023-05-01 10:49:36 +0200

    Add .net project and test pipeline

--- Prog/Program.cs
+++ Prog/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! The answer is ");
+Console.WriteLine($"Hello, World! The answer is {CoreLib.MyTools.Answer()} ");
