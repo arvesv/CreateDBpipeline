045a43c3920e2457a94569028ea5e2ce5757bfb4
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2023-05-09 21:47:54 +0200

    ww

--- CoreLib/MyTools.cs
+++ CoreLib/MyTools.cs
@@ -1,7 +1,24 @@
-﻿namespace CoreLib
+﻿using Microsoft.Data.SqlClient;
+
+namespace CoreLib
 {
     public class MyTools
     {
         public static int Answer() => 42;
+
+
+        public static string[] DbWho(string connectionString)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = "sp_who;";
+
+
+
+            return Array.Empty<string>();
+
+
+        }
     }
+
 }
