cd5d84a35114471ed8ac4518703d0207dfbfdacf
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2023-05-15 01:18:27 +0200

    Deploy a database in a container instance and talk to it (#8)
    Azure ACI database

--- CoreLib/MyTools.cs
+++ CoreLib/MyTools.cs
@@ -1,7 +1,23 @@
-﻿namespace CoreLib
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace CoreLib
 {
     public class MyTools
     {
         public static int Answer() => 42;
+
+
+        public static IEnumerable<string> DbWho(string connectionString)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            var dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = "sp_databases;";
+            var reader = dbCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            while (reader.Read())
+                yield return reader.GetString("database_name");
+        }
     }
+
 }
--- Prog/Program.cs
+++ Prog/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+
+
+
+
+
+
+var builder = new ConfigurationBuilder()
+    .AddEnvironmentVariables();
+
+var configuration = builder.Build();
+
+
+string? s = configuration["dbconn"];
+
+
+
+
+
+
+
 Console.WriteLine($"Hello, World! The answer is {CoreLib.MyTools.Answer()} ");
+
+var z = CoreLib.MyTools.DbWho(s);
+
+var y = z.ToArray();
+Console.WriteLine(y);
+
+
